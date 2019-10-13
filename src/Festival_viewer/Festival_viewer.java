a630cf536e012aca3e34cb9c294d2612749f92c8
Darkside-PC <s3506883@student.rmit.edu.au>
2019-10-13 19:14:49 +0900

    implement function to build bands
    implement function to build bands
    more used reference added

--- src/Festival_viewer/Festival_viewer.java
+++ src/Festival_viewer/Festival_viewer.java
@@ -5,8 +5,10 @@
  */
 package Festival_viewer;
 
+import Viewer_lib.Band;
 import Viewer_lib.Festival;
 import Viewer_lib.recordBuilder;
+import static Viewer_lib.recordBuilder.formatOutput;
 import java.util.List;
 
 
@@ -18,6 +20,8 @@ import java.util.List;
  * https://stackoverflow.com/questions/20534862/how-to-efficiently-map-a-org-json-jsonobject-to-a-pojo
  * https://stackoverflow.com/questions/23479332/import-json-url-to-java-and-parse-it-using-jackson-library
  * http://tutorials.jenkov.com/java-json/jackson-objectmapper.html#jackson-objectmapper-example
+ * http://tutorials.jenkov.com/java-json/jackson-annotations.html
+ * https://winterbe.com/posts/2014/07/31/java8-stream-tutorial-examples/
  * 
  */
 public class Festival_viewer {
@@ -27,7 +31,8 @@ public class Festival_viewer {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        List<Festival> records = recordBuilder.buildRecord();
-        System.out.print(records.toString());
+        List<Festival> festivals = recordBuilder.apiRequester();
+        List<Band> bands = recordBuilder.buildBands(festivals);
+        formatOutput(bands);
     }
 }
--- src/Viewer_lib/recordBuilder.java
+++ src/Viewer_lib/recordBuilder.java
@@ -1,31 +1,33 @@
-package Viewer_lib;
-
-
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import java.io.IOException;
-import java.net.URL;
-import java.util.List;
-
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+package Viewer_lib;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.IOException;
+import java.net.URL;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  *
  * @author Darkside-PC
  */
-@JsonIgnoreProperties(ignoreUnknown = true)
+
 public class recordBuilder {
-    public static List<Festival> buildRecord(){
+    //request data from swagger api
+    public static List<Festival> apiRequester(){
         ObjectMapper objMapper = new ObjectMapper();
         List<Festival> festivals = null;
         String urlString = (String)("http://eacodingtest.digital.energyaustralia.com.au/"
                 + "api/v1/festivals");
         
+        //get json array to list of festival object
         try{
             URL targetUrl = new URL(urlString);
             festivals = objMapper.readValue(targetUrl, new TypeReference<List<Festival>>() {});
@@ -36,4 +38,55 @@ public class recordBuilder {
         return festivals;
     }
     
+    //build band from festivals list and order by record label
+    public static List<Band> buildBands(List<Festival> festivals){
+        //parse festival list into list of bands and sort by record label
+        List<Band> bands = null;
+        bands = festivals
+                .stream()
+                .flatMap(Festival -> Festival.getBands().stream())
+                .sorted(Comparator.comparing(Band::getRecordLabel))
+                .collect(Collectors.toList());
+        
+        //retrive list of festival for band    
+        for(Band band : bands){
+            List<String> festList = null;
+            festList = festivals
+                    .stream()
+                    .filter(Festival -> Festival.getBands().contains(band))    
+                    .map(Festival::getName)
+                    .collect(Collectors.toList());
+            //sort festival list
+            Collections.sort(festList);
+            //assign festival list to band
+            band.getFestivals().addAll(festList);
+        }
+        
+        return bands;
+    }
+    
+    //format output string to ignore duplicate record label
+    public static void formatOutput(List<Band> bands){
+        int defaultIndex = 0;
+        String defaultLabel = "[Not Available]";
+        String lastLabel = bands.get(defaultIndex).getRecordLabel();
+        
+        for(Band band : bands){
+            String currLabel = band.getRecordLabel();
+            
+            //compare label name
+            if(currLabel.equals("")){
+                band.setRecordLabel(defaultLabel);
+                System.out.println(band.toString());
+            }else if(currLabel.equals(lastLabel)){
+                
+                System.out.println(band.toStringWithOutLabel());
+            }else{
+                System.out.println(band.toString());
+            }
+            
+            //update label name
+            lastLabel = currLabel;
+        }
+    }
 }
